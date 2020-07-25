a3ad4698f588a31086fd7a3eb8b2bf8bf101ed11
toyboot4e <toyboot4e@gmail.com>
2020-07-25 11:34:01 +0900

    add missing mojo.rs

---
+++ fna3d/src/mojo.rs
@@ -0,0 +1,12 @@
+//! Aliases
+use fna3d_sys as sys;
+
+// FNA3D.h does NOT provide concrete definition
+// pub type Effect = sys::MOJOSHADER_effect;
+// pub type EffectTechnique = sys::MOJOSHADER_effectTechnique;
+// pub type EffectStateChanges = sys::MOJOSHADER_effectStateChanges;
+
+// So look at `mojoshader.h`
+pub type Effect = sys::mojo::MOJOSHADER_effect;
+pub type EffectTechnique = sys::mojo::MOJOSHADER_effectTechnique;
+pub type EffectStateChanges = sys::mojo::MOJOSHADER_effectStateChanges;
