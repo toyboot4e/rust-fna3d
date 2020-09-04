44a6c14b7b3518c1d8f3a9793252a562c3da30de
toyboot4e <toyboot4e@gmail.com>
2020-09-04 14:50:04 +0900

    refine fna3d::img limiting input

--- fna3d/src/img.rs
+++ fna3d/src/img.rs
@@ -1,9 +1,51 @@
-//! `FNA3D_Image.h`
+//! Wrapper of `FNA3D_Image.h`
 //!
-//! Currently the implementation is pretty crappy and should NOT be used. Prefer other bindings to
-//! `stb_image` or use SDL's RW struct. Or maybe use `image` crate.
+//! Iternally, `FNA3D_Image` uses [`stb_image`] (`stbi`) with callback functions and it enables
+//! arbitrary IO. However, I'm limiting the input to `Path` because there's a problem if the
+//! `Read` is `BufRead` (e.g. `BufReader`). I don't know how to deal with it for now. See
+//! `self::StbiCallbackState::read` for details.
+//!
+//! You also can use [`stb_image`] directory without callbacks or other libraries such as RWops in
+//! SDL2.
+//!
+//! [`stb_image`]: https://github.com/nothings/stb/blob/master/stb_image.h
+//!
+//! # Example (presudo code)
+//!
+//! ```
+//! pub struct MyTexure2D {
+//!     raw: fna3d::Texture2D,
+//!     w: u32,
+//!     h: u32,
+//! }
+//!
+//! impl MyTexture2D {
+//!     pub fn from_path(
+//!         device: &mut fna3d::Device,
+//!         path: impl AsRef<std::path::Path>,
+//!     ) -> Option<Self> {
+//!         let (pixels_ptr, len, [w, h]) = fna3d::img::from_path(path, None);
+//!         if pixels_ptr == std::ptr::null_mut() {
+//!             return None;
+//!         }
+//!
+//!         let texture = { /* load texture 2D */ }l
+//!
+//!         fna3d::img::free(pixels_ptr as *mut _);
+//!
+//!         return Some(texture);
+//!     }
+//! }
+//! ```
 
 use fna3d_sys as sys;
+
+use std::{
+    fs::File,
+    io::{Read, Seek, SeekFrom},
+    os::raw::c_void,
+    path::Path,
+};
 
 /// Callback used to pull data from the stream
 pub type ReadFunc = sys::FNA3D_Image_ReadFunc;
@@ -14,55 +56,63 @@ pub type SkipFunc = sys::FNA3D_Image_SkipFunc;
 /// Callback used to check that we're reached the end of a stream
 pub type EofFunc = sys::FNA3D_Image_EOFFunc;
 
-use std::{
-    io::{Read, Seek, SeekFrom},
-    os::raw::c_void,
-};
-
-/// Decodes PNG/JPG/GIF data into raw RGBA8 texture data from an arbitrary IO
+/// Decodes PNG/JPG/GIF data into raw RGBA8 texture data from an path
 ///
-/// Returns nullptr if failed.
-///
-/// FNA3D_Image uses `stb_image` (`stbi`) and it uses callback functions to enable arbitrary IO.
-///
-/// You may want to wrap a `Read` struct with `std::io::BufReader`. Be sure to free the memory with
-/// `FNA3D_Image_Free` after use!
-pub fn load_image_from_reader<R: Read + Seek>(
-    reader: R,
+/// The return type is not `Vec` becaue frees its memory when dropping. Be sure to free the memory
+/// with `FNA3D_Image_Free` after use!
+pub fn from_path(
+    path: impl AsRef<Path>,
     force_size: Option<[u32; 2]>,
-    do_zoom: bool,
-) -> (*mut u8, usize, [u32; 2]) {
+) -> (*const u8, u32, [u32; 2]) {
+    let path = path.as_ref();
+    let reader = File::open(path)
+        .ok()
+        .unwrap_or_else(|| panic!("failed to open file {}", path.display()));
+
     let context = StbiCallbackState {
         reader,
         is_end: false,
     };
+
     unsafe {
         self::load(
-            Some(StbiCallbacks::<R>::read),
-            Some(StbiCallbacks::<R>::skip),
-            Some(StbiCallbacks::<R>::eof),
+            Some(StbiCallbacks::<File>::read),
+            Some(StbiCallbacks::<File>::skip),
+            Some(StbiCallbacks::<File>::eof),
             std::mem::transmute(&context),
             force_size,
-            do_zoom,
         )
     }
 }
+
+/// Free pixels loaded with [`from_reader`]
+///
+/// [`from_reader`]: ./fn.from_reader.html
+pub fn free(mem: *mut u8) {
+    unsafe {
+        sys::FNA3D_Image_Free(mem);
+    }
+}
+
+// --------------------------------------------------------------------------------
+// impls
 
 struct StbiCallbackState<R: Read + Seek> {
     reader: R,
     is_end: bool,
 }
 
-/// Callbacks for `stb_image` used via `FNA3D_Image`
+/// Callback functions for `FNA3D_Image.h`, i.e. `stb_image.h`
 struct StbiCallbacks<R: Read + Seek> {
     phantom: std::marker::PhantomData<R>,
 }
 
 impl<R: Read + Seek> StbiCallbacks<R> {
-    // If we want to remove this transumute function, we (only) have to modify the output of
-    // bindgen. But I prefered to leave it and use `transmute`.
+    /// Casts unknown pointer to the state type
+    ///
+    /// If we want to remove this transumute function, we (only) have to modify the output of
+    /// bindgen. But I prefered to leave it.
     unsafe fn transmute_cx(cx: *mut c_void) -> *mut StbiCallbackState<R> {
-        // TODO: error if it's an invalid pointer
         std::mem::transmute(cx)
     }
 
@@ -70,26 +120,30 @@ impl<R: Read + Seek> StbiCallbacks<R> {
     unsafe extern "C" fn read(
         context: *mut ::std::os::raw::c_void,
         out_ptr: *mut ::std::os::raw::c_char,
-        out_size: i32,
+        size: i32,
     ) -> i32 {
         let cx = &mut *Self::transmute_cx(context);
 
-        let out = std::slice::from_raw_parts_mut(out_ptr as *mut u8, out_size as usize);
-        // FIXME: this is a hack
-        let len_read = cx.reader.read(out).unwrap() as u32;
-        // let len_read = if out_size > 8064 {
+        let out = std::slice::from_raw_parts_mut(out_ptr as *mut u8, size as usize);
+
+        let len_read = cx.reader.read(out).unwrap() as i32;
+
+        // FIXME: This is a hack required for `BufReader`. Maybe it is not safe and I'm limiting
+        //        `from_path`.
+        // let len_read = if size > 8064 {
         //     cx.reader.read_exact(out).unwrap();
-        //     out_size
+        //     size
         // } else {
         //     cx.reader.read(out).unwrap() as i32
         // };
 
-        log::trace!("stbi readFunc: {} -> {}", out_size, len_read);
+        log::trace!("stbi readFunc: {} -> {}", size, len_read);
 
         len_read as i32
-        // out_size
     }
 
+    /// FIXME: is this OK?
+    ///
     /// Skips `n` bytes
     unsafe extern "C" fn skip(context: *mut ::std::os::raw::c_void, n: i32) {
         log::trace!("stbi skipFunc: {}", n);
@@ -99,16 +153,13 @@ impl<R: Read + Seek> StbiCallbacks<R> {
             .unwrap_or_else(|err| panic!("error in anf skip func {}", err));
     }
 
-    // TODO: do we have to peek??
+    /// FIXME: is this OK?
     unsafe extern "C" fn eof(context: *mut ::std::os::raw::c_void) -> i32 {
         let cx = &mut *Self::transmute_cx(context);
         log::trace!("stbi eofFunc: {}", cx.is_end);
         cx.is_end as i32
     }
 }
-
-// --------------------------------------------------------------------------------
-// impls
 
 /// Decodes PuncG/JPG/GIF data into raw RGBA8 texture data.
 ///
@@ -133,14 +184,15 @@ unsafe fn load(
     eof_fn: EofFunc,
     context: *mut ::std::os::raw::c_void,
     force_size: Option<[u32; 2]>,
-    do_zoom: bool, // TODO: need this? (or use force_size.is_some()?)
-) -> (*mut u8, usize, [u32; 2]) {
-    let (mut w, mut h, mut len) = (0, 0, 0);
+) -> (*const u8, u32, [u32; 2]) {
+    let do_zoom = force_size.is_some();
     let force_size = if let Some([x, y]) = force_size {
         [x as i32, y as i32]
     } else {
         [-1, -1]
     };
+
+    let (mut w, mut h, mut len) = (0, 0, 0);
     let pixels = sys::FNA3D_Image_Load(
         read_fn,
         skip_fn,
@@ -153,42 +205,6 @@ unsafe fn load(
         force_size[1],
         do_zoom as u8,
     );
-    (pixels, len as usize, [w as u32, h as u32])
+
+    (pixels, len as u32, [w as u32, h as u32])
 }
-
-// extern "C" {
-//     pub fn FNA3D_Image_Free(mem: *mut u8);
-// }
-
-// pub type FNA3D_Image_WriteFunc = ::std::option::Option<
-//     unsafe extern "C" fn(
-//         context: *mut ::std::os::raw::c_void,
-//         data: *mut ::std::os::raw::c_void,
-//         size: i32,
-//     ),
-// >;
-
-// extern "C" {
-//     pub fn FNA3D_Image_SavePNG(
-//         writeFunc: FNA3D_Image_WriteFunc,
-//         context: *mut ::std::os::raw::c_void,
-//         srcW: i32,
-//         srcH: i32,
-//         dstW: i32,
-//         dstH: i32,
-//         data: *mut u8,
-//     );
-// }
-
-// extern "C" {
-//     pub fn FNA3D_Image_SaveJPG(
-//         writeFunc: FNA3D_Image_WriteFunc,
-//         context: *mut ::std::os::raw::c_void,
-//         srcW: i32,
-//         srcH: i32,
-//         dstW: i32,
-//         dstH: i32,
-//         data: *mut u8,
-//         quality: i32,
-//     );
-// }
