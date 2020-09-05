2646078f4b81d34a83dc7fef4ea7e487735392ee
toyboot4e <toyboot4e@gmail.com>
2020-09-05 22:45:03 +0900

    fix fna3d::img
    considering Read::read does not always try to read as much as possible

--- fna3d/src/img.rs
+++ fna3d/src/img.rs
@@ -1,14 +1,11 @@
 //! Wrapper of `FNA3D_Image.h`
 //!
-//! Iternally, `FNA3D_Image` uses [`stb_image`] (`stbi`) with callback functions and it enables
-//! arbitrary IO. However, I'm limiting the input to `Path` because there's a problem if the
-//! `Read` is `BufRead` (e.g. `BufReader`). I don't know how to deal with it for now. See
-//! `self::StbiCallbackState::read` for details.
-//!
-//! You also can use [`stb_image`] directory without callbacks or other libraries such as RWops in
-//! SDL2.
+//! Iternally, `FNA3D_Image` uses [`stb_image`] (`stbi`) with callback functions where arbitrary
+//! IO is allowed. You also can use [`stb_image`] directory without callbacks or other libraries
+//! such as [`SDL_RWops`] in SDL2.
 //!
 //! [`stb_image`]: https://github.com/nothings/stb/blob/master/stb_image.h
+//! [`SDL_RWops`]: https://wiki.libsdl.org/SDL_RWops
 //!
 //! # Example (presudo code)
 //!
@@ -25,14 +22,13 @@
 //!         path: impl AsRef<std::path::Path>,
 //!     ) -> Option<Self> {
 //!         let (pixels_ptr, len, [w, h]) = fna3d::img::from_path(path, None);
+//!
 //!         if pixels_ptr == std::ptr::null_mut() {
 //!             return None;
 //!         }
 //!
 //!         let texture: MyTexture2D = unimplemented!();
-//!
 //!         fna3d::img::free(pixels_ptr as *mut _);
-//!
 //!         return Some(texture);
 //!     }
 //! }
@@ -42,8 +38,8 @@ use fna3d_sys as sys;
 
 use std::{
     fs::File,
-    io::{Read, Seek, SeekFrom},
-    os::raw::c_void,
+    io::{self, BufReader, Read, Seek, SeekFrom},
+    os::raw::{c_char, c_void},
     path::Path,
 };
 
@@ -56,10 +52,14 @@ pub type SkipFunc = sys::FNA3D_Image_SkipFunc;
 /// Callback used to check that we're reached the end of a stream
 pub type EofFunc = sys::FNA3D_Image_EOFFunc;
 
-/// Decodes PNG/JPG/GIF data into raw RGBA8 texture data from an path
+/// Decodes PNG/JPG/GIF data into raw RGBA8 texture data
 ///
-/// The return type is not `Vec` becaue frees its memory when dropping. Be sure to free the memory
-/// with `FNA3D_Image_Free` after use!
+/// Be sure to [`free`] the returned memory after use!
+///
+/// The return type is not [`Vec`] because it frees its content when dropping.
+///
+/// [`free`]: crate::img::free
+/// [`Vec`]: std::vec::Vec
 pub fn from_path(
     path: impl AsRef<Path>,
     force_size: Option<[u32; 2]>,
@@ -68,7 +68,22 @@ pub fn from_path(
     let reader = File::open(path)
         .ok()
         .unwrap_or_else(|| panic!("failed to open file {}", path.display()));
+    let reader = BufReader::new(reader); // FIXME: is this good?
+    self::from_reader(reader, force_size)
+}
 
+/// Decodes PNG/JPG/GIF data into raw RGBA8 texture data
+///
+/// Be sure to [`free`] the returned memory after use!
+///
+/// The return type is not [`Vec`] because it frees its content when dropping.
+///
+/// [`free`]: crate::img::free
+/// [`Vec`]: std::vec::Vec
+pub fn from_reader<R: Read + Seek>(
+    reader: R,
+    force_size: Option<[u32; 2]>,
+) -> (*const u8, u32, [u32; 2]) {
     let context = StbiCallbackState {
         reader,
         is_end: false,
@@ -76,18 +91,16 @@ pub fn from_path(
 
     unsafe {
         self::load(
-            Some(StbiCallbacks::<File>::read),
-            Some(StbiCallbacks::<File>::skip),
-            Some(StbiCallbacks::<File>::eof),
+            Some(StbiCallbacks::<R>::read),
+            Some(StbiCallbacks::<R>::skip),
+            Some(StbiCallbacks::<R>::eof),
             std::mem::transmute(&context),
             force_size,
         )
     }
 }
 
-/// Free pixels loaded with [`from_reader`]
-///
-/// [`from_reader`]: ./fn.from_reader.html
+/// Frees pixels loaded with [`from_reader`](from_reader) or [`from_path`](from_path)
 pub fn free(mem: *mut u8) {
     unsafe {
         sys::FNA3D_Image_Free(mem);
@@ -107,35 +120,40 @@ struct StbiCallbacks<R: Read + Seek> {
     phantom: std::marker::PhantomData<R>,
 }
 
-impl<R: Read + Seek> StbiCallbacks<R> {
-    /// Casts unknown pointer to the state type
-    ///
-    /// If we want to remove this transumute function, we (only) have to modify the output of
-    /// bindgen. But I prefered to leave it.
-    unsafe fn transmute_cx(cx: *mut c_void) -> *mut StbiCallbackState<R> {
-        std::mem::transmute(cx)
+/// Tries to fill the buffer as much as possible. `Ok` if we read any byte
+///
+/// We need this helper because both [`read`] and [`read_exact`] don't match our needs. [`read`]
+/// does not always try to read as much as possible, while `read_exact` returns error when it is
+/// not able to fullfill the given buffer.
+///
+/// The implementation is based on [`read_exact`].
+///
+/// [`read`]: std::io::Read::read
+/// [`read_exact`]: std::io::Read::read_exact
+fn read_as_much(mut reader: impl Read, mut buf: &mut [u8]) -> io::Result<usize> {
+    let mut read = 0;
+    while !buf.is_empty() {
+        match reader.read(buf) {
+            Ok(0) => break,
+            Ok(n) => {
+                let tmp = buf;
+                buf = &mut tmp[n..];
+                read += n;
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::Interrupted => {}
+            Err(e) => return Err(e),
+        }
     }
+    Ok(read)
+}
 
+impl<R: Read + Seek> StbiCallbacks<R> {
     /// Reads up to `size` bytes
-    unsafe extern "C" fn read(
-        context: *mut ::std::os::raw::c_void,
-        out_ptr: *mut ::std::os::raw::c_char,
-        size: i32,
-    ) -> i32 {
-        let cx = &mut *Self::transmute_cx(context);
+    unsafe extern "C" fn read(context: *mut c_void, out_ptr: *mut c_char, size: i32) -> i32 {
+        let cx = &mut *(context as *mut StbiCallbackState<R>);
 
         let out = std::slice::from_raw_parts_mut(out_ptr as *mut u8, size as usize);
-
-        let len_read = cx.reader.read(out).unwrap() as i32;
-
-        // FIXME: This is a hack required for `BufReader`. Maybe it is not safe and I'm limiting
-        //        `from_path`.
-        // let len_read = if size > 8064 {
-        //     cx.reader.read_exact(out).unwrap();
-        //     size
-        // } else {
-        //     cx.reader.read(out).unwrap() as i32
-        // };
+        let len_read = self::read_as_much(&mut cx.reader, out).unwrap();
 
         log::trace!("stbi readFunc: {} -> {}", size, len_read);
 
@@ -145,23 +163,23 @@ impl<R: Read + Seek> StbiCallbacks<R> {
     /// FIXME: is this OK?
     ///
     /// Skips `n` bytes
-    unsafe extern "C" fn skip(context: *mut ::std::os::raw::c_void, n: i32) {
+    unsafe extern "C" fn skip(context: *mut c_void, n: i32) {
         log::trace!("stbi skipFunc: {}", n);
-        let cx = &mut *Self::transmute_cx(context);
+        let cx = &mut *(context as *mut StbiCallbackState<R>);
         cx.reader
             .seek(SeekFrom::Current(n as i64))
             .unwrap_or_else(|err| panic!("error in anf skip func {}", err));
     }
 
-    /// FIXME: is this OK?
-    unsafe extern "C" fn eof(context: *mut ::std::os::raw::c_void) -> i32 {
-        let cx = &mut *Self::transmute_cx(context);
-        log::trace!("stbi eofFunc: {}", cx.is_end);
+    /// FIXME: is this OK? I've never seen it's called and really not confident
+    unsafe extern "C" fn eof(context: *mut c_void) -> i32 {
+        let cx = &mut *(context as *mut StbiCallbackState<R>);
+        log::trace!("stbi eofFunc: is_end={}", cx.is_end);
         cx.is_end as i32
     }
 }
 
-/// Decodes PuncG/JPG/GIF data into raw RGBA8 texture data.
+/// Decodes PNG/JPG/GIF data into raw RGBA8 texture data.
 ///
 /// * `read_fn`
 ///   Callback used to pull data from the stream.
@@ -182,7 +200,7 @@ unsafe fn load(
     read_fn: ReadFunc,
     skip_fn: SkipFunc,
     eof_fn: EofFunc,
-    context: *mut ::std::os::raw::c_void,
+    context: *mut c_void,
     force_size: Option<[u32; 2]>,
 ) -> (*const u8, u32, [u32; 2]) {
     let do_zoom = force_size.is_some();
